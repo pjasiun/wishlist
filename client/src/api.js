aaa444742f9233fb984688daf4e7b968d879c3b0
Piotr Jasiun <piotr.jasiun@glovoapp.com>
2024-09-10 22:26:01 +0200

    Load only first 5 images.

--- client/src/api.js
+++ client/src/api.js
@@ -1,23 +1,23 @@
 export async function getData() {
-    const response = await fetch('https://laura.jasiun.pl/data/')
-    if (!response.ok) {
-      throw new Error('Network response was not ok.')
-    }
-    const data = await response.json()
+  const response = await fetch('https://laura.jasiun.pl/data/')
+  if (!response.ok) {
+    throw new Error('Network response was not ok.')
+  }
+  const data = await response.json()
 
-    await Promise.all(
-      data.map(
-        ({ image }) =>
-          new Promise((resolve, reject) => {
-            const img = new Image()
-            img.onload = () => resolve(img)
-            img.onerror = img.onabort = () => reject(image)
-            img.src = image
-          })
-      )
+  await Promise.all(
+    data.slice(0, 5).map(
+      ({ image }) =>
+        new Promise((resolve, reject) => {
+          const img = new Image()
+          img.onload = () => resolve(img)
+          img.onerror = img.onabort = () => reject(image)
+          img.src = image
+        })
     )
+  )
 
-    return data;
+  return data
 }
 
 export async function setBooking(pageId, booker) {
